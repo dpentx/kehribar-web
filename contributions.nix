f1e0ad1bbe1b248e2ca7aecfb0b11d641c87e3ad
Kehribar <103407696+dpentx@users.noreply.github.com>
2026-02-18 20:18:12 +0300

    Create contributions.nix

---
+++ contributions.nix
@@ -0,0 +1,10 @@
+# Katkıda bulunduğum açık kaynak projeler
+# Yeni proje eklemek için bu bloğu kopyala ve düzenle
+
+Metrolist = {
+  url = "https://github.com/metrolist/metrolist";
+  icon = "icons/metrolist.png";
+  desc = "Android Youtube Music Clienti";
+  desc_en = "Youtube Music Client for Android";
+  type = "translation";
+};
